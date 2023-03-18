93a65a5d4984968d7a1866e0705166a0a0b410ec
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-03-18 18:18:24 -0400

    adds noteList component

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,44 +1,70 @@
-import { useState } from "react";
-import { Row, Col, Stack, Button, Form } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { useMemo, useState } from "react";
+import { Row, Col, Stack, Button, Form, Card, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
+import styles from "./NoteList.module.css";
 
 type NoteListProps = {
-    availableTags: Tag[]
-}
+  availableTags: Tag[];
+  notes: SimplifiedNote[];
+};
 
-const NoteList = ({availableTags}: NoteListProps) => {
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-  return <>
-    <Row>
-        <Col><h1>Notes</h1></Col>
-        <Col xs="auto">
-            <Stack gap={2} direction="horizontal">
-                <Link to="/new">
-                    <Button variant="primary">Create</Button>
-                </Link>
-                <Button variant="outline-secondary">Edit Tags</Button>
-            </Stack>
+type SimplifiedNote = {
+  tags: Tag[];
+  title: string;
+  id: string;
+};
+
+const NoteList = ({ availableTags, notes }: NoteListProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectedTags, notes]);
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
         </Col>
-    </Row>
-    <Form>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
         <Row className="mb-4">
-            <Col>
-                <Form.Group controlId="title">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group>
-            </Col>
-            <Col>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+          <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <ReactSelect
                 value={selectedTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
-                options={availableTags.map(tag => {
-                  return {label: tag.label, value:tag.id}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
                 })}
                 onChange={(tags) => {
                   setSelectedTags(
@@ -52,8 +78,35 @@ const NoteList = ({availableTags}: NoteListProps) => {
             </Form.Group>
           </Col>
         </Row>
-    </Form>
-  </>
-}
+      </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default NoteList
+const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
+  console.log(tags);
+  return (
+    <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+      <Card.Body>
+        <Stack gap={2} className="align-items-center justify-content-center h-100">
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+              {tags.map(tag => (
+                  <Badge className="text-truncate" key={tag.id}>{tag.label}</Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+};
+export default NoteList;
