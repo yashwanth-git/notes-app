c836286efeef22b15311f232079683a8acf38eda
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-03-18 20:47:07 -0400

    adds note component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
 import NoteList from "./NoteList";
+import NoteLayout from "./NoteLayout";
+import Note from "./Note";
 
 export type Note = {
   id: string;
@@ -73,8 +75,8 @@ const App = () => {
               />
             }
           />
-          <Route path="/:id">
-            <Route index element={<h1>Show</h1>} />
+          <Route path="/:id" element={<NoteLayout notes={noteWithTags} />}>
+            <Route index element={<Note/>} />
             <Route path="edit" element={<h1>Edit</h1>} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
